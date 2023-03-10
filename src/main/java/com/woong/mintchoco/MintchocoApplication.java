b1d45bc678015b0d519b31511c20dabd466bebcb
Woong99 <dndry1072@naver.com>
2023-03-10 20:41:23 +0900

    [feat] : 프로젝트 생성

---
+++ src/main/java/com/woong/mintchoco/MintchocoApplication.java
@@ -0,0 +1,13 @@
+package com.woong.mintchoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintchocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintchocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woong/mintchoco/MintchocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woong.mintchoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintchocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
